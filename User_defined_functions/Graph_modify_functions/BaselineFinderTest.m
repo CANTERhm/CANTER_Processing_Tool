a6f48d1fa4d23d1776d7c59f90e3ed2f4639eba2
meatpunsher <fleischh@hm.edu>
2019-01-25 15:59:21 +0100

    neuer Baselinefinder im Test

---
+++ User_defined_functions/Graph_modify_functions/BaselineFinderTest.m
@@ -0,0 +1,18 @@
+function [baseline_edges]=BaselineFinderTest(x,y)
+
+y = smoothdata(y, 'Gaussian', 100);
+y_grad = gradient(y);
+
+y_grad = y_grad/min(y_grad);
+y_grad = smoothdata(y_grad, 'Gaussian', 100);
+baseline = find(0.1>y_grad & y_grad>-0.1);
+
+if size(baseline,2) > 300
+    baseline = baseline(50:end-50);
+end
+
+baseline_edges(1, 1) = baseline(1);
+baseline_edges(1, 2) = baseline(end);
+baseline_edges(1, 3) = baseline(end);
+
+end
