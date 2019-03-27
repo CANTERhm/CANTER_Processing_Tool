7c3b6bdaa6b9435f164d010adc918eec4f75010a
BastianHartmann <bastian.hartmann@hm.edu>
2019-03-27 16:10:17 +0100

    Creation of edit field for selecting the percentage value the "contact point via Hertz fit" function uses

--- User_defined_functions/Graph_modify_functions/ContactPoint.m
+++ User_defined_functions/Graph_modify_functions/ContactPoint.m
@@ -12,26 +12,27 @@ y_linfit = polyval(p, x);
 
 % Check how the contact point shell be found
 SelectedMode = handles.btngroup_contact.SelectedObject.String;
-if strcmp(SelectedMode, 'via intersection')
-    % Get the intersection point of the baseline and the graph
-    contactpoint = find(y-y_linfit <= 0, 1, 'last');
-    
-    % Set the contactpoint as 0/0
-    x_corrected = x-(x(contactpoint));
-else
-    % Get first the intersection point of the baseline and the graph
-    contactpoint = find(y-y_linfit <= 0, 1, 'last');
-    
-    % Set the preliminar contactpoint as 0/0
-    x_corrected = x-(x(contactpoint));
-    
-    %Fit a Hertz_fit to find the CP via linear approximation
-    perc_steps = 20;   
-    [~,d_h,gof] = initial_guess_hard(x_corrected,y,(perc_steps),17.5,0.5,'plot','off');
-    
-    
-    % Set the new contactpoint as 0/0
-    x_corrected = x_corrected-d_h;
+switch SelectedMode
+    case 'via intersection'
+        % Get the intersection point of the baseline and the graph
+        contactpoint = find(y-y_linfit <= 0, 1, 'last');
+
+        % Set the contactpoint as 0/0
+        x_corrected = x-(x(contactpoint));
+    case 'via Hertz fit'
+        % Get first the intersection point of the baseline and the graph
+        contactpoint = find(y-y_linfit <= 0, 1, 'last');
+
+        % Set the preliminar contactpoint as 0/0
+        x_corrected = x-(x(contactpoint));
+
+        %Fit a Hertz_fit to find the CP via linear approximation
+        perc_steps = str2double(handles.contact_percentage_hertz.String);   
+        [~,d_h,gof] = initial_guess_hard(x_corrected,y,(perc_steps),17.5,0.5,'plot','off');
+
+
+        % Set the new contactpoint as 0/0
+        x_corrected = x_corrected-d_h;
 end
 
 end
