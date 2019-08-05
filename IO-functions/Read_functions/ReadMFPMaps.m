3c4ddc35d67b828b31a380df4c8fba7c12ac3c1a
BastianHartmann <bastian.hartmann@hm.edu>
2019-08-05 17:33:56 +0200

    Function: ReadMFPMaps 1. Integration of read_WaveNotes function 2. Calculation of the y-channel is now in dependency of the underlying data unit of the laoded mfp map 3. The values for the y-cahnnel calculation and the Index for separating extend and retract part are now extractend from the wave notes.

--- IO-functions/Read_functions/ReadMFPMaps.m
+++ IO-functions/Read_functions/ReadMFPMaps.m
@@ -30,26 +30,42 @@ end
     
     fileroot = fullfile(pathname,files{i}); % part for the actual IBW file
     data = IBWread(fileroot); % import the actual IBW file
+    
+    % read detailed Map information
     [info_cell,additional_parameters] = read_WaveNotes(data);
+    dataUnits = data.waveHeader.dataUnits;
+    InvOLS = additional_parameters.InvOLS;
+    SpringConst = additional_parameters.SpringConstant;
+    
+
     x_data_raw = data.y(:,1).*(-1); % write x values from IBW file to x_data
+    switch dataUnits
+        case 'V'    % Deflection in Volts can be written directly to output.
+            y_data_raw = data.y(:,2);
+        case 'm'    % Deflection in meter has to be converted to Volts by Deviding with the "inverted optical lever sensitivity (InvOLS)".
+            y_data_raw = data.y(:,2)./InvOLS; 
+        case 'N'    % The applied force in Newton has to be converted by Dividing with the InvOLS and the spring constant.
+            y_data_raw = data.y(:,2)./(InvOLS*SpringConst);
+        otherwise
+            y_data_raw = data.y(:,2); % Otherwise just write data to output, but warn the user about the unknown data unit.
+            warning('Unknown data unit!\ny-channel is likely to be in the wrong unit!!!!%s',' ');
+    end
+            
+    % last index of extend part
+    extend_end_ind = additional_parameters.Indexes(2);
     
-    %% replace 1e7 with corresponding value from WaveNotes!!!!!!!!!!!!!!!!
-    y_data_raw = data.y(:,2)*1e7; % write y values from IBW file to y_data
-    
-    
-    % find maximum of force curve to separate the retract part and the extend part
-    %% replace max(y_data_raw) and I with index from WaveNotes!!!!!!!!!!!!!!!!
-    [~,I] = max(y_data_raw); % find maximum (M) and index of Maximum (I) of y_data
-    x_data.(Forcecurve_count{i}) = x_data_raw(1:I,1); % keep the extend part
-    y_data.(Forcecurve_count{i}) = y_data_raw(1:I,1); % keep the extend part
-    x_data_retract.(Forcecurve_count{i}) = x_data_raw(I+1:end,1); % keep the retract part
-    y_data_retract.(Forcecurve_count{i}) = y_data_raw(I+1:end,1); % keep the retract part
+    % separate the retract part and the extend part
+    x_data.(Forcecurve_count{i}) = x_data_raw(1:extend_end_ind,1); % keep the extend part
+    y_data.(Forcecurve_count{i}) = y_data_raw(1:extend_end_ind,1); % keep the extend part
+    x_data_retract.(Forcecurve_count{i}) = x_data_raw(extend_end_ind+1:end,1); % keep the retract part
+    y_data_retract.(Forcecurve_count{i}) = y_data_raw(extend_end_ind+1:end,1); % keep the retract part
     
     
     %% updating the wait bar
     waitbar(i/(length(files)));
 
  end
+
 % write the output variable mfpmapdata
 mfpmapdata{1} = info_cell;
 mfpmapdata{2} = additional_parameters;
