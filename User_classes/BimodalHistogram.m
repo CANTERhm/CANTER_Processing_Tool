5d2ece5cccd022eea689cb381e965342289d5ea6
BastianHartmann <bastian.hartmann@hm.edu>
2019-10-11 17:20:10 +0200

    Finalisation of the BimodalHistogram class - Added general instructions for the use of the class, the various properties, and the methods. - Added handling of NaNs in the EModul vector. - Finished implementation of the "AddAnnotations" method.

--- User_classes/BimodalHistogram.m
+++ User_classes/BimodalHistogram.m
@@ -1,32 +1,67 @@
 classdef BimodalHistogram
-    %UNTITLED2 Summary of this class goes here
-    %   Detailed explanation goes here
+    %BimodalHistogram: Class for the displaying of Young's modulus
+    %histograms and fitting of a bimodal distribution.
+    %
+    % General  instructions:
+    %
+    %   1) Create a BimodalHistogram object (e.g. BiHist) by calling the
+    %      constructor with a vector of the Young's moduli (EModul).
+    %      -> For a more detailed help click on the "BimodalHistogram" link
+    %         under "Constructor Summary"
+    %
+    %           Example:
+    %           BiHist = BimodalHistogram(EModul,[0 200e3],100,'yes');
+    %
+    %   2) Fit a bimodal distribution on the histogram data and plot the 
+    %      resulting curve, as well as the two single Gaussian
+    %      distributions, the bimodal distribution consists of, to the 
+    %      histogram by calling the "doFit" method of the BiHist object.
+    %      -> For more details click on the "doFit" link in the "Method 
+    %         Summary" section bellow.
+    %    
+    %           Example:
+    %           BiHist = BiHist.doFit;
+    %
+    %   3) You can add proper axis labels and an annotation showing the
+    %      two peak positions of the bimodal fit by calling the 
+    %      "AddAnnotations" method of the BiHist object.
+    %      -> For more details click on the "AddAnnotations" link in the
+    %         "Method Summary" section below.
+    %
+    %           Example:
+    %           BiHist = BiHist.AddAnnotations;
+    %
+    %   -> For any further information use the links below regarding the
+    %      properties and methods of the BimodalHistogram class.
+    
     
     properties
-        EModul = [];
-        hist = [];
-        edges = [];
-        BinNum = [];
-        BinCenters = [];
-        BinWidth = [];
-        BinCounts = [];
-        gauss1 = @(a1,E1,w1,x)a1.*exp(-((x-E1)./w1).^2);
-        gauss2 = @(a2,E2,w2,x)a2.*exp(-((x-E2)./w2).^2);
-        bimodal = @(a1,E1,w1,a2,E2,w2,x)a1.*exp(-((x-E1)./w1).^2)+a2.*exp(-((x-E2)./w2).^2);
-        fit_obj = [];
-        parameters = struct('a1',[],'E1',[],'w1',[],'a2',[],'E2',[],'w2',[]);
-        initialGuess = struct('a1',[],'E1',[],'w1',[],'a2',[],'E2',[],'w2',[]);
-        x_data_fit = [];
-        y_data_fit = [];
-        y_gauss1 = [];
-        y_gauss2 = [];
+        EModul = [];        % Numeric vector containing the Young's modulus values.
+        hist = [];          % The histogram handle is here available after the histogram is plotted.
+        edges = [];         % Bin edges of the histogram as a numeric vector.
+        BinNum = [];        % Number of histogram bins.
+        BinCenters = [];    % Centers of the histogram bins as numeric vector (used as x values for the bimodal fit).
+        BinWidth = [];      % Width of histogram bins.
+        BinCounts = [];     % Numeric vector containing the heigths of the histogram bins (used as y values for the bimodal fit).
+        gauss1 = @(a1,E1,w1,x)a1.*exp(-((x-E1)./w1).^2);	% Function handle of the first Gaussian distribution.
+        gauss2 = @(a2,E2,w2,x)a2.*exp(-((x-E2)./w2).^2);	% Function handle of the secon Gaussian distribution.
+        bimodal = @(a1,E1,w1,a2,E2,w2,x)a1.*exp(-((x-E1)./w1).^2)+a2.*exp(-((x-E2)./w2).^2);    % Function handle of the bimodal distribution.   
+        fit_obj = [];       % The fit object is available here after the "doFit" method was executed on the BiHist object.
+        parameters = struct('a1',[],'E1',[],'w1',[],'a2',[],'E2',[],'w2',[]);   % Struct containing the fittet values of the bimodal distribution are stored.
+        initialGuess = struct('a1',[],'E1',[],'w1',[],'a2',[],'E2',[],'w2',[]); % Struct containing the initial guesses for the fit of the bimodal distribution.
+        x_data_fit = [];    % Numeric vector of the x values used to draw the fit results.
+        y_data_fit = [];    % Numeric vector of the y values used to draw the fit result of the bimodal distribution.
+        y_gauss1 = [];      % Numeric vector of the y values used to draw the fit result of the first Gaussian distribution.
+        y_gauss2 = [];      % Numeric vector of the y values used to draw the fit result of the second Gaussian distribution.
+        annotation_obj = [];    % Handle to the annotation shown in the plot window after exectuing the "AddAnnotations" method.
         
 
     end
     
     methods
         function obj = BimodalHistogram(EModul,x_range,BinNum,plot_arg)
-            % obj = BimodalHistrogram(EModul,x_range,BinNum,plot_arg);
+            % Constructor of the BimodalHistogram class
+            % obj = BimodalHistrogram(EModul,x_range,BinNum,[plot_arg]);
             %
             % BimodalHistogram constructs an instance of this class
             %
@@ -47,6 +82,7 @@ classdef BimodalHistogram
             %                          histogram afterward by using the
             %                          plotHist method.
             
+            
             % input check of constructor
             if nargin < 3
                error('Too few input arguments!\nPlease enter at least EModul, x_range, and BinNum.\n%s',' '); 
@@ -63,6 +99,7 @@ classdef BimodalHistogram
             if any(EModul<0)
                 error('EModul must be only positive numbers!');
             end
+            
             
             if ~isnumeric(x_range)
                 error('x_range must be numeric!');
@@ -89,7 +126,7 @@ classdef BimodalHistogram
             end
             
             % calculate and assign property values
-            obj.EModul = EModul;
+            obj.EModul = EModul(~isnan(EModul));
 
             edges = linspace(x_range(1),x_range(2),BinNum+1);
             obj.edges = edges';
@@ -122,20 +159,33 @@ classdef BimodalHistogram
         end
         
         function obj = doFit(obj,StartPoints,plot_arg)
-            % obj = doFit(obj,StartPoints,plot_arg,varargin);
+            % Fit the bimodal distribution to the histogram data and plot the fit result.
+            % 
+            % Syntax:
+            %   * obj = doFit(obj);
+            %   * obj = obj.doFit;
+            %   * obj = doFit(obj[,StartPoints,plot_arg]);
+            %   * obj = obj.doFit([StartPoints,plot_arg]);
             % 
             % doFit This method calculates the bimodal fit and draws the
             % fit together with the unimodal distributions
-            % input:
-            %       - obj           -> BimodalHistogram object.
-            %       - StartPoints   -> Start values for the fiting of the
+            %
+            % Input:
+            %       * obj           -> BimodalHistogram object.
+            %       * StartPoints   -> Start values for the fiting of the
             %                          six parameters of the bimodal
             %                          distribution [a1 E1 w1 a2 E2 w2].
-            %       - plot_arg      -> 'yes' (default) if you whant the
+            %                          If no StartPoints are defined, the
+            %                          doFit method uses its own algorithm
+            %                          for finding the initial guesses for
+            %                          the six fitting parameters.
+            %       * plot_arg      -> 'yes' (default) if you whant the
             %                          fits to be plotted.
             %                          'no' if you don't want the fits to
             %                          be plotted. You can plot the fits
             %                          anytime by using the plotFit method.
+            
+            
             
             % error handling for doFit method            
             if ~exist('plot_arg','var')
@@ -228,7 +278,9 @@ classdef BimodalHistogram
             
         end
         
-        function obj = plotHist(obj)          
+        function obj = plotHist(obj)
+            % Plot the histogram
+            %
             % obj = plotHist(obj)
             %
             % plotHist: Method to plot the Histogram of the Young's modulus
@@ -236,13 +288,21 @@ classdef BimodalHistogram
             % The binning is determined by the property "edges".
             %
             % Syntax:
-            % * obj = plotHist(obj);
-            % * obj = obj.plotHist();
+            %   * obj = plotHist(obj);
+            %   * obj = obj.plotHist();
+            %   * plotHist(obj);
+            %   * obj.plotHist;
             
             obj.hist = histogram(obj.EModul,obj.edges);
         end
         
         function plotFit(obj)
+            % Plot the fitted bimodal distributions together with the two Gaussian distributions.
+            % 
+            % Syntax:
+            %   * plotFit(obj)
+            %   * obj.plotFit;
+            
             % error handling for plotFit method
             if isempty(obj.x_data_fit) || isempty(obj.y_data_fit) || isempty(obj.y_gauss1) || isempty(obj.y_gauss2)
                 error('First you need to apply the "doFit" method on this BimodalHistogram object!');
@@ -257,6 +317,8 @@ classdef BimodalHistogram
         end
         
         function showInitialGuess(obj)
+            % Draw the bimodal distribution with the initial guesses for the six fit parameters.
+            %
             % obj = showInitialGuess(obj)
             %
             % showInitialGuess draws the initialGuess curve in the current
@@ -287,7 +349,12 @@ classdef BimodalHistogram
             
         end
         
-        function AddAnnotations(obj)
+        function obj = AddAnnotations(obj)
+            % Add axis labels and annotation with the peak positions to the current axes.
+            %
+            % Syntax:
+            %   * obj = AddAnnotations(obj);
+            %   * obj = obj.AddAnnotations;
             
             % get order of magnitude of the displayed range
             max_edge = obj.edges(end);
@@ -315,8 +382,8 @@ classdef BimodalHistogram
             tick_numbers = str2double(tick_text);
             tick_numbers_new = tick_numbers/10^power;
             current_ax.XTickLabel = num2cell(string(tick_numbers_new));
-            xlabel(current_ax,sprintf('Young''s modulus [%s]',axis_unit));
-            ylabel(current_ax,'Frequency');
+            xlabel(current_ax,sprintf('Young''s modulus [%s]',axis_unit),'FontSize',14);
+            ylabel(current_ax,'Frequency','FontSize',14);
             
             % determine order of magnetude for E1 and E2 and set the
             % corresponding unit
@@ -359,22 +426,33 @@ classdef BimodalHistogram
                                         E2_unit);
                                     
             % add annotation to figure
+            annot = annotation('textbox','String',annotation_string,'FontSize',14,'Visible','off');
+            annot_size = annot.Position;
+            ax_pos = current_ax.Position;
+            annot.Position(1) = (ax_pos(1) + ax_pos(3)) - (annot_size(3)+0.05);
+            annot.Position(2) = (ax_pos(2) + ax_pos(4)) - (annot_size(4)+0.05);
+            annot.Visible = 'on';
+            
+            obj.annotation_obj = annot;
             
             
         end
         
         function obj = GenerateTestEModul(obj)
+            % Generate test distribution.
+            %
             % obj = GenerateTestEModul(obj)
             %
             % GenerateTestEModul generates a EModul vector for the purpose
-            % of testing this class. ATTENTION: When you apply this method
-            % on a BimodalHistogram class the "EModul" property will be
-            % overwritten!
+            % of testing this class.
+            %   ATTENTION: When you apply this method on a BimodalHistogram
+            %              class the "EModul" property will be overwritten!
             %
             % One possibility for testing is to first create a
             % BimodalHistogram object with a random EModul vector and a
-            % x_range of [0 300e3]. For the BinNum 75 is a good guess. h1 =
-            % BimodalHistogram(rand(5,1),[0 300e3],75);
+            % x_range of [0 300e3]. For the BinNum 75 is a good guess.
+            %
+            %   -> h1 = BimodalHistogram(rand(5,1),[0 300e3],75);
             %
             % Then you can apply the "GenerateTestEModul" method to
             % overwrite the random EModul vector and reinitialize the
