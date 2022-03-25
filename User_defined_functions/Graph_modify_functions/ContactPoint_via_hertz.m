981e45d71bf3dff60b6e18f415faa3ac6e3bda37
BastianHartmann <bastian.hartmann@hm.edu>
2022-03-25 18:02:55 +0100

    The contact point via hetz fit now only considers the force curve up to the set end of fit.

--- User_defined_functions/Graph_modify_functions/ContactPoint_via_hertz.m
+++ User_defined_functions/Graph_modify_functions/ContactPoint_via_hertz.m
@@ -22,17 +22,35 @@ function [x_corrected]=ContactPoint_via_hertz(x, y, baseline_edges, handles,vara
         perc_steps = varargin{1};
     end
     
+    % Limit curve values to the maximum fit depth
+    fit_depth = str2double(handles.hertz_fit_depth.String);
+    fit_depth_unit = handles.text46.String;
+    switch fit_depth_unit
+        case "mm"
+            fit_depth = fit_depth * 1e-3;
+        case "µm"
+            fit_depth = fit_depth * 1e-6;
+        case "nm"
+            fit_depth = fit_depth * 1e-9;
+        case "pm"
+            fit_depth = fit_depth * 1e-12;
+    end
+    
+    x_mask = x_corrected >= fit_depth*(-1);
+    x_fit = x_corrected(x_mask);
+    y_fit = y(x_mask);
+
     switch handles.tip_shape
         case 'four_sided_pyramid'
             % approximation and calculation of contact point via liniarized
             % hertz model
-            [~,d_h,~] = initial_guess_hard(x_corrected,y,perc_steps,handles.tip_angle,handles.poisson,'plot','off');
+            [~,d_h,~] = initial_guess_hard(x_fit,y_fit,perc_steps,handles.tip_angle,handles.poisson,'plot','off');
         case 'flat_cylinder'
             % get contact point via polyfit on part of the negative curve
             % section
-            x_min = min(x_corrected);
-            poly_mask = x_corrected < (x_min - perc_steps/100*x_min);
-            p = polyfit(x_corrected(poly_mask),y(poly_mask),1);
+            x_min = min(x_fit);
+            poly_mask = x_fit < (x_min - perc_steps/100*x_min);
+            p = polyfit(x_fit(poly_mask),y_fit(poly_mask),1);
             d_h = roots(p);
     end
 
