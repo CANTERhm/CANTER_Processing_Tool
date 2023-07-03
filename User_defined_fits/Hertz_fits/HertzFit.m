9934abbac7d0c6c48a1437465686c52e178c2c22
BastianHartmann <bastian.hartmann@hm.edu>
2023-07-03 11:33:37 +0200

    Implementation of the fit funcitons for a three-sided pyramid and cone as indender geometries.

--- User_defined_fits/Hertz_fits/HertzFit.m
+++ User_defined_fits/Hertz_fits/HertzFit.m
@@ -5,7 +5,7 @@ function [EModul,varargout] = HertzFit(x,y,fit_start,d_int,tip_shape,indenter_va
 % varargout{1} = gof; goodness of fit = rsquare
 % varargout{2} = fittedcurve_x; X data of the fitted curve
 % varargout{3} = fittedcurve_y; Y data of the fitted curve
-% varargout{4} = contact point; JUST AVAILABLE WHEN THE TIP_SHAPE IS
+% varargout{4} = contact point; ONLY AVAILABLE WHEN THE TIP_SHAPE IS
 %                               FLAT_CYLINDER
 
 %% Code
@@ -16,13 +16,19 @@ function [EModul,varargout] = HertzFit(x,y,fit_start,d_int,tip_shape,indenter_va
 switch tip_shape
     case 'four_sided_pyramid'
         angle = indenter_value;
-        func_string = sprintf('(tan(%f.*pi/180)/(2*(1-%f.^2)).*x.^2)',angle,poisson);
-        ft = fittype( {func_string}, 'independent', 'x', 'dependent', 'y', 'coefficients', {'EModul'} );
+        func_string = 'x.^2';
+        ft = fittype( {func_string}, 'independent', 'x', 'dependent', 'y', 'coefficients', {'A'} );
         opts = fitoptions( 'Method', 'LinearLeastSquares' );
     case 'three_sided_pyramid'
-        keyboard;
+        angle = indenter_value;
+        func_string = 'x.^2';
+        ft = fittype({func_string}, 'independent','x','dependent','y','coefficients',{'A'});
+        opts = fitoptions('Method','LinearLeastSquares');
     case 'cone'
-        keyboard;
+        angle = indenter_value;
+        func_string = 'x.^2';
+        ft = fittype({func_string}, 'independent','x','dependent','y','coefficients',{'A'});
+        opts = fitoptions('Method','LinearLeastSquares');
     case 'flat_cylinder'
         radius = indenter_value;
         ft = fittype("poly1");
@@ -46,11 +52,15 @@ opts.Exclude = excludedPoints;
 switch tip_shape
     case 'four_sided_pyramid'
         baseline_mask = xData<0;
-        EModul = fitresult.EModul;
+        EModul = (2.*fitresult.A.*(1-poisson.^2))./tand(angle);
 
     case 'three_sided_pyramid'
+        baseline_mask = xData<0;
+        EModul = (fitresult.A.*(1-poisson.^2))./(0.8887.*tand(angle));
 
     case 'cone'
+        baseline_mask = xData<0;
+        EModul = (fitresult.A.*pi.*(1-poisson.^2))./(2.*tand(angle));
 
     case 'flat_cylinder'
         m = fitresult.p1;
@@ -58,15 +68,17 @@ switch tip_shape
         contact_point = -1*(t/m);
         baseline_mask = xData<contact_point;
         EModul = (m*(1-poisson^2))/(2*radius);
+
 end
-fittedcurve_y = zeros(length(xData),1);
+
+fittedcurve_y = zeros(length(xData),1); 
 switch tip_shape
     case 'four_sided_pyramid'
-        fittedcurve_y(baseline_mask) = EModul*((tan(angle.*pi/180)/(2*(1-poisson.^2)).*(xData(baseline_mask).^2)));
+        fittedcurve_y(baseline_mask) = fitresult.A.*(xData(baseline_mask).^2);
     case 'three_sided_pyramid'
-
+        fittedcurve_y(baseline_mask) = fitresult.A.*(xData(baseline_mask).^2);
     case 'cone'
-
+        fittedcurve_y(baseline_mask) = fitresult.A.*(xData(baseline_mask).^2);
     case 'flat_cylinder'
         fittedcurve_y(baseline_mask) = EModul*(2*radius./(1-poisson.^2)).*(xData(baseline_mask)-contact_point);
 end
